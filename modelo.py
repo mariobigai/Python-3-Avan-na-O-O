c87431b9e845bb179aa805d50aa6d23edbf4848a
mariobigai <mariobigai1@gmail.com>
2023-02-02 19:56:51 -0300

    retirando método imprime e adicionando dunde str nas classes filhas

--- modelo.py
+++ modelo.py
@@ -24,16 +24,16 @@ class Filme(Programa):
         super().__init__(nome, ano)
         self.duracao = duracao
 
-    def imprime(self):
-        print(f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}')
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}'
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-    def imprime(self):
-        print(f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}')
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -48,4 +48,4 @@ atlanta.dar_likes()
 lista = [atlanta, vingadores]
 
 for programa in lista:
-    programa.imprime()
+    print(programa)
